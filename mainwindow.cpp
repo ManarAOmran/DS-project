5ace99588d4890226517e68b4645531fc08ee16c
Eslam Mohamed <70959607+EslamMohmed@users.noreply.github.com>
2022-02-08 01:24:32 +0200

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,13 +9,14 @@
 #include "node.h"
 #include "xml_tree.h"
 #include <QTextStream>
-
+#include <QDir>
 using namespace std;
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->label->hide();
 }
 
 MainWindow::~MainWindow()
@@ -143,13 +144,13 @@ void MainWindow::on_CompressionButton_clicked()
         str.append(QString:: fromStdString(to_string(c)));
 
     QString fileName = QFileDialog::getSaveFileName(this,
-                       tr("Save Address Book"), "",
-                       tr("COMP (*.comp);;All Files ()"));
+                                                    tr("Save Address Book"), "",
+                                                    tr("COMP (*.comp);;All Files ()"));
     QFile newDoc(fileName);
     if(newDoc.open(QIODevice::WriteOnly)) {
         QTextStream out(&newDoc);
-            out<<str;
-            newDoc.flush();
+        out<<str;
+        newDoc.flush();
     }
 
     newDoc.close();
@@ -176,7 +177,84 @@ void MainWindow::on_XmlToJson_clicked()
 }
 
 
+void MainWindow::on_graph_clicked()
+{
+    std::string xml_string=ui->input_text->toPlainText().toStdString();
+    Node *root = new Node("XML", "");
+    parse_tree(root, xml_string);
+    root = root->children[0];
+    vector<User> users;
+    for(int i =0;i<root->children.size();i++){
+        User temp_user;
+        Node* user_node= root->children[i];
+        for(int j=0;j<user_node->children.size();j++){
+            Node* user_data = user_node->children[j];
+            if(user_data->name == "name")
+                temp_user.name = user_data->data;
+            else if(user_data->name == "id")
+                temp_user.id = stoi(user_data->data);
+            else if(user_data->name == "followers")
+            {
+                for(uint k=0;k<user_data->children.size();k++){
+                    int temp_id = stoi(user_data->children[k]->children[0]->data);
+                    temp_user.followers.push_back(temp_id);
+                }
+            }
+        }
+        users.push_back(temp_user);
+    }
+    graph* graph1=new graph();
+    for(int i=0;i<users.size();i++){
+        graph1->add_vertex();
+        for(int j=0;j<users[i].followers.size();j++){
+            graph1->add_edge(users[i].id,users[i].followers[j]);
+        }
+    }
+    QString temp_out;
+    for(int i=1;i<graph1->numVertices;i++){
 
+        for(int j=1;j<graph1->numVertices;j++){
+            temp_out +=QString::number(graph1->vertArr[i][j]) + "    ";
+        }
+        temp_out+='\n';
+    }
+    ui->output_text->setPlainText(temp_out);
+    //show dot file
+    string result = "digraph Graph {\n\n";
+    for(const auto &user : users)
+    {
+        result += "\t" + to_string(user.id) + " -> {";
+        for(size_t i = 0; i < user.followers.size(); i++)
+        {
+            if(i)
+                result += " ,";
+            result += to_string(user.followers[i]);
+        }
+        result += "}\n";
+    }
+    result += "}\n\n";
+    QFile file("C:\\Users\\EGYPT\\OneDrive\\Desktop\\graph.dot");
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Text)){
+        return;
+    }
+    QTextStream out(&file);
+    QString text = QString::fromStdString(result);
+    out << text;
+    file.flush();
+    file.close();
+    QProcess process;
+    process.start("dot -Tpng -O graph.dot", QStringList() << "C:\\Users\\EGYPT\\OneDrive\\Desktop");
 
+}
 
+void MainWindow::on_showgraph_clicked()
+{
+    ui->label->show();
+    QPixmap mypix("C:\\Users\\EGYPT\\OneDrive\\Desktop\\graph.dot.png");
+    ui->label->setPixmap(mypix);
+}
 
+void MainWindow::on_pushButton_clicked()
+{
+    ui->label->hide();
+}
